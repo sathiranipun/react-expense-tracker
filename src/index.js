7f1580118914d5103141ed4e2b2df0b487f28529
sathiranipun <sathira.n@intervest.lk>
2024-05-03 17:36:30 +0530

    redux persist store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import { App } from "./App";
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <PersistGate persistor={persistor}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </PersistGate>
   </Provider>
 );
